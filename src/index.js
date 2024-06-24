1d4cbedc0b0ff09297f4e23715561a09961541ca
grahpanchal <grahpanchal3110@gmail.com>
2024-06-24 18:37:09 +0530

    Utils code discusstion

--- src/index.js
+++ src/index.js
@@ -9,7 +9,15 @@ dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`App is listening on port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO  db connection failed !!!", err);
+  });
 
 // import express from "express";
 
